7b873a416dca6bb43c17b20eb070855999a86687
CanTung <cantung678@gmail.com>
2026-09-24 22:44:42 +0700

    chuẩn hoá file csv

--- Hieu/dataset_master_corpus/convert_data.py
+++ Hieu/dataset_master_corpus/convert_data.py
@@ -14,12 +14,18 @@ with open(input_file, "r", encoding="utf-8") as infile, \
 
         id_ = data["id"]
         text = data["text"]
-        label = data["is_scam"]
+        is_scam = data["is_scam"]
 
-        # Escape dấu " nếu text có chứa
+        # Chuyển nhãn
+        if is_scam == 0:
+            label = "legitimate"
+        else:
+            label = "scam"
+
+        # Escape dấu " trong text
         text = text.replace('"', '""')
 
-        # Chỉ cột text được bao bởi một cặp "
+        # Chỉ cột text có dấu "
         outfile.write(f'{id_},"{text}",{label}\n')
 
 print("Đã chuyển thành công!")
